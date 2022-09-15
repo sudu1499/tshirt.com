0179ba1adfc85937aad84b542c2d3c513ee10e53
sudarshan n <suduanagha@gmail.com>
2022-09-15 23:39:59 +0530

    finally completed cart thing

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,HttpResponse,redirect
 from .models import cart
 from product_list.models import products
+from django.contrib.auth.models import User
 # Create your views here.
-def cart(request):
+def cart_f(request):
     if request.user.is_authenticated:
         return render(request,'cart.html')
     else:
@@ -11,16 +12,24 @@ def cart(request):
 
 def add_to_cart(request):
     if request.POST:
-        # print("$$$$",request.POST['product_id'])
-        # pid=products.objects.get(id=request.POST['product_id'])
-        # print(pid)
-        print('request.user.username ',request.user.username)
-        print("request.POST['product_id'] ",request.POST['product_id'])
-        obj=Cart.objects.create(cart_id=request.POST['product_id'],user_name=request.user.username)
-        # obj.cart_id=request.POST['product_id']
-        # obj.user_name=request.user.username
+        print("username=",request.user.username)
+        print("product_id=",request.POST['product_id'])
+        print("size=",request.POST['size'])
+        print("coubnt=",request.POST['count'])
+        username=request.user.username
+        pid=request.POST['product_id']
+        size=request.POST['size']
+        pcnt=request.POST['count']
+        usn=User.objects.all().filter(username=username)
+        # print(usn[0].username)
+        obj=cart.objects.create(user_name=usn[0],product_count=pcnt,size=size)
+        obj.product_id.set(pid)
         obj.save()
-        print('added ')
+        print('done')
+        # obj.user_name=username
+        # obj.product_id=pid
+        # obj.product_count=pcnt
+        # obj.size=size
         return render(request,'cart.html')
 
     else:
